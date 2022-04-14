6e0afc1f85f3ea43d830faee664845edb4aa900f
Pablo Armando <pabloar24.7@hmail.com>
2022-04-14 09:34:45 -0300

    3 dots added

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -2,18 +2,41 @@ import './sliderStyle.css'
 import imagen1 from './slider1.jpg'
 import imagen2 from './slider2.jpg'
 import imagen3 from './slider3.jpg'
+import { useState } from 'react'
 
 const Slider = () => {
+   const images = [{slider : imagen1}, {slider : imagen2}, {slider : imagen3}]
+
+  const [slideIndex, setSlideIndex] = useState(1)
+  const moveDot = index => {
+    setSlideIndex(index)
+}
+
   return (
     <>
      <div className="sliderContainer">
         <div className="slider">
             <ul>
-               <li><img src={imagen1} alt='imagen1' className='imageSlider'></img></li>
-               <li><img src={imagen2} alt='imagen2' className='imageSlider'></img></li>
-               <li><img src={imagen3} alt='imagen3' className='imageSlider'></img></li>
+                {images.map((image, index) => {
+                    
+                    return (
+                        <li key={image.index} className={slideIndex === index + 1 ? "slide active-anim" : "slide"}>
+                            <img src={image.slider} alt="Jenny Panichi" className="logoImg"/>
+                        </li>
+                    )
+                })}
+              <div className="container-dots">
+                {Array.from({length: 3}).map((item, index) => (
+                    <div 
+                    onClick={() => moveDot(index + 1)}
+                    className={slideIndex === index + 1 ? "dot active" : "dot"}
+                    ></div>
+                ))}
+            </div>    
             </ul>
-         </div>   
+           
+         </div>
+         
      </div>
     </>
   )
