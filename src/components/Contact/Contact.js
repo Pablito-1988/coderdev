bd79bde3411232fb28484b92fef663a61ee66a84
Pablo Armando <pabloar24.7@hmail.com>
2022-05-02 17:08:01 -0300

    emailjs working

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,8 +2,27 @@ import React from 'react';
 import portada from '../../assets/img/contact.png';
 import contactImg from '../../assets/img/contactImg.png';
 import "./Contact.css";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const service = process.env.REACT_APP_SERVICE
+  const template = process.env.REACT_APP_TEMPLATE
+  const user = process.env.REACT_APP_USER
+  
+  function sendEmail(e){
+    e.preventDefault();
+
+emailjs.sendForm(service,template, e.target,user)
+  .then((result) => {
+    console.log(result.text);
+  }, (error) => {
+      console.log(error.text);
+  });
+  e.target.reset()
+  
+ 
+
+}
   return<>
   <div className='contactContiner'>
       <div className="portada">
@@ -14,12 +33,12 @@ const Contact = () => {
             <h2 className='formTitle'>DEJAME TU MENSAJE</h2>
             <div className="formContainer">
               <p className='formQuote'>Utilice el siguiente formulario para enviarme su consulta</p>
-              <form className="form">
+              <form className="form"  onSubmit={sendEmail}>
                 <div className='nameAndEmailContainer'>
-                  <input className='nameInput' type="text"  placeholder='Nombre' required/>
-                <input  className='emailInput'type="email" name="" id="" placeholder='Correo electrónico' required />
+                  <input className='nameInput' type="text" name='fullName'  placeholder='Nombre' required/>
+                <input  className='emailInput'type="email" name='email' id="" placeholder='Correo electrónico' required />
                 </div>
-                <textarea name="" id="" cols="30" rows="10" placeholder='Mensaje' required></textarea>
+                <textarea name="" id="" cols="30" name='text' rows="10" placeholder='Mensaje' required></textarea>
                 <div className="submitButton">
                   <button className='button' type="submit">Entregar</button>
                 </div>
