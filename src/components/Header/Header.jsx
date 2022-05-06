4c805229a4605d47b8e9447a1f09758f43699eba
Luz Malgieri <luzmalgieri@gmail.com>
2022-05-06 16:16:07 -0300

    Responsive navbar half-way done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./Header.css";
 import MainLogo from "../../assets/img/diamond.png";
 import Dropdown from "../Dropdown/Dropdown";
@@ -10,55 +10,67 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   const { t } = useTranslation(["header"]);
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
   useEffect(() => {
     if (localStorage.getItem("i18nextLng")?.length > 2) {
       i18next.changeLanguage("es");
-      
     }
   }, []);
 
   return (
-    <div>
+    <nav className="navbar">
       <div className="topDivHeader">
         <div id="leftbox">
-          <Link to="/">
-            <img src={MainLogo} alt="Jenny Panichi" className="logoImg" />
+          <Link to="/" onClick={closeMobileMenu}>
+            <img src={MainLogo} alt="Jenny Panichi" className="logoHeader" />
           </Link>
         </div>
         <div id="middlebox">
-          <SearchBar placeholder="Buscar..."/>
+          <SearchBar placeholder="Buscar..." />
         </div>
         <div id="rightbox">
           <Dropdown />
         </div>
+        <div className="mobile-menu-icon" onClick={handleClick}>
+          {/* <i> {click ? <FiX /> : <FiMenu />} </i> */}
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
       </div>
-      <div className="divBcg">
-        <nav>
-          <ul className="menuBar">
-            <li key={t("entrevistas.nombre")} className="menuItem">
-              <Link to={t("entrevistas.ref")}>
-                {t("entrevistas.nombre")}
-                <br></br>
-                {/* <span className="hoverCircle"></span> */}
-              </Link>
+      
+        <ul
+          className={click ? "nav-links-mobile" : "nav-links"}
+          onClick={closeMobileMenu}
+        >
+          {click ? (
+            <li className="menuItem" onClick={closeMobileMenu}>
+              <Dropdown />{" "}
             </li>
-            <li key={t("peleadores.nombre")} className="menuItem">
-              <Link to={t("peleadores.ref")}>{t("peleadores.nombre")}</Link>
-            </li>
-            <li key={t("calendario.nombre")} className="menuItem">
-              <Link to={t("calendario.ref")}>{t("calendario.nombre")}</Link>
-            </li>
-            <li key={t("quiensoy.nombre")} className="menuItem">
-              <Link to={t("quiensoy.ref")}>{t("quiensoy.nombre")}</Link>
-            </li>
-            <li key={t("contacto.nombre")} className="menuItem">
-              <Link to={t("contacto.ref")}>{t("contacto.nombre")}</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </div>
+          ) : null}
+          <li key={t("entrevistas.nombre")} className="menuItem">
+            <Link to={t("entrevistas.ref")} onClick={closeMobileMenu}>
+              {t("entrevistas.nombre")}
+              <br></br>
+            </Link>
+          </li>
+          <li key={t("peleadores.nombre")} className="menuItem">
+            <Link to={t("peleadores.ref")} onClick={closeMobileMenu}>{t("peleadores.nombre")}</Link>
+          </li>
+          <li key={t("calendario.nombre")} className="menuItem">
+            <Link to={t("calendario.ref")} onClick={closeMobileMenu}>{t("calendario.nombre")}</Link>
+          </li>
+          <li key={t("quiensoy.nombre")} className="menuItem">
+            <Link to={t("quiensoy.ref")} onClick={closeMobileMenu}>{t("quiensoy.nombre")}</Link>
+          </li>
+          <li key={t("contacto.nombre")} className="menuItem">
+            <Link to={t("contacto.ref")} onClick={closeMobileMenu}>{t("contacto.nombre")}</Link>
+          </li>
+        </ul>
+      
+    </nav>
   );
 };
 
